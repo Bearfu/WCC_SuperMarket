4f99528dafc04b7cd7adbba1bc6366ffe8c76c95
Bear_fu <303761829@qq.com>
2016-08-22 20:55:28 +0800

    添加订单管理测试demo

--- WCC_SuperMarket/Order/views.py
+++ WCC_SuperMarket/Order/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
+
+def Test(request):
+    return HttpResponse("Hello Now_a_Magic")
